302a23f232f6a1aafc53f04c60d78413b6a973ca
simkaa47 <simkaa7@gmail.com>
2022-04-01 15:10:05 +0300

    Написан контрол для трендов

--- HMI_Плотномер/Views/Resourses/UserControls/TrendControl.xaml.cs
+++ HMI_Плотномер/Views/Resourses/UserControls/TrendControl.xaml.cs
@@ -48,7 +48,7 @@ namespace IDensity.Views.Resourses.UserControls
             DependencyProperty.Register("LineVisibility1", typeof(Visibility), typeof(TrendControl), new PropertyMetadata(Visibility.Visible));
         #endregion
 
-        #region Навзание тренда
+        #region Навзание тренда 1
         public string LineTitle1
         {
             get { return (string)GetValue(LineTitle1Property); }
@@ -63,7 +63,229 @@ namespace IDensity.Views.Resourses.UserControls
 
         #endregion
 
+        #region Видимость тренда 2
+        public Visibility LineVisibility2
+        {
+            get { return (Visibility)GetValue(LineVisibility2Property); }
+            set { SetValue(LineVisibility2Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineVisibility1.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineVisibility2Property =
+            DependencyProperty.Register("LineVisibility2", typeof(Visibility), typeof(TrendControl), new PropertyMetadata(Visibility.Visible));
+        #endregion
+
+        #region Навзание тренда 2
+        public string LineTitle2
+        {
+            get { return (string)GetValue(LineTitle2Property); }
+            set { SetValue(LineTitle2Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineTitle1.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineTitle2Property =
+            DependencyProperty.Register("LineTitle2", typeof(string), typeof(TrendControl), new PropertyMetadata(""));
 
 
+
+        #endregion
+
+        #region Видимость тренда 3
+        public Visibility LineVisibility3
+        {
+            get { return (Visibility)GetValue(LineVisibility3Property); }
+            set { SetValue(LineVisibility3Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineVisibility1.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineVisibility3Property =
+            DependencyProperty.Register("LineVisibility3", typeof(Visibility), typeof(TrendControl), new PropertyMetadata(Visibility.Visible));
+        #endregion
+
+        #region Навзание тренда 3
+        public string LineTitle3
+        {
+            get { return (string)GetValue(LineTitle3Property); }
+            set { SetValue(LineTitle3Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineTitle1.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineTitle3Property =
+            DependencyProperty.Register("LineTitle3", typeof(string), typeof(TrendControl), new PropertyMetadata(""));
+
+
+
+        #endregion
+
+        #region Видимость тренда 4
+        public Visibility LineVisibility4
+        {
+            get { return (Visibility)GetValue(LineVisibility4Property); }
+            set { SetValue(LineVisibility4Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineVisibility4.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineVisibility4Property =
+            DependencyProperty.Register("LineVisibility4", typeof(Visibility), typeof(TrendControl), new PropertyMetadata(Visibility.Visible));
+        #endregion
+
+        #region Навзание тренда 4
+        public string LineTitle4
+        {
+            get { return (string)GetValue(LineTitle4Property); }
+            set { SetValue(LineTitle4Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineTitle4.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineTitle4Property =
+            DependencyProperty.Register("LineTitle4", typeof(string), typeof(TrendControl), new PropertyMetadata(""));
+
+
+
+        #endregion
+
+        #region Видимость тренда 5
+        public Visibility LineVisibility5
+        {
+            get { return (Visibility)GetValue(LineVisibility5Property); }
+            set { SetValue(LineVisibility5Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineVisibility5.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineVisibility5Property =
+            DependencyProperty.Register("LineVisibility5", typeof(Visibility), typeof(TrendControl), new PropertyMetadata(Visibility.Visible));
+        #endregion
+
+        #region Навзание тренда 5
+        public string LineTitle5
+        {
+            get { return (string)GetValue(LineTitle5Property); }
+            set { SetValue(LineTitle5Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineTitle4.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineTitle5Property =
+            DependencyProperty.Register("LineTitle5", typeof(string), typeof(TrendControl), new PropertyMetadata(""));
+        #endregion
+
+        #region Видимость тренда 6
+        public Visibility LineVisibility6
+        {
+            get { return (Visibility)GetValue(LineVisibility6Property); }
+            set { SetValue(LineVisibility6Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineVisibility6.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineVisibility6Property =
+            DependencyProperty.Register("LineVisibility6", typeof(Visibility), typeof(TrendControl), new PropertyMetadata(Visibility.Visible));
+        #endregion
+
+        #region Навзание тренда 6
+        public string LineTitle6
+        {
+            get { return (string)GetValue(LineTitle6Property); }
+            set { SetValue(LineTitle6Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineTitle6.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineTitle6Property =
+            DependencyProperty.Register("LineTitle6", typeof(string), typeof(TrendControl), new PropertyMetadata(""));
+        #endregion
+
+        #region Видимость тренда 7
+        public Visibility LineVisibility7
+        {
+            get { return (Visibility)GetValue(LineVisibility7Property); }
+            set { SetValue(LineVisibility7Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineVisibility7.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineVisibility7Property =
+            DependencyProperty.Register("LineVisibility7", typeof(Visibility), typeof(TrendControl), new PropertyMetadata(Visibility.Visible));
+        #endregion
+
+        #region Навзание тренда 7
+        public string LineTitle7
+        {
+            get { return (string)GetValue(LineTitle7Property); }
+            set { SetValue(LineTitle7Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineTitle7.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineTitle7Property =
+            DependencyProperty.Register("LineTitle7", typeof(string), typeof(TrendControl), new PropertyMetadata(""));
+        #endregion
+
+        #region Видимость тренда 8
+        public Visibility LineVisibility8
+        {
+            get { return (Visibility)GetValue(LineVisibility8Property); }
+            set { SetValue(LineVisibility8Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineVisibility8.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineVisibility8Property =
+            DependencyProperty.Register("LineVisibility8", typeof(Visibility), typeof(TrendControl), new PropertyMetadata(Visibility.Visible));
+        #endregion
+
+        #region Навзание тренда 8
+        public string LineTitle8
+        {
+            get { return (string)GetValue(LineTitle8Property); }
+            set { SetValue(LineTitle8Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineTitle8.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineTitle8Property =
+            DependencyProperty.Register("LineTitle8", typeof(string), typeof(TrendControl), new PropertyMetadata(""));
+        #endregion
+
+        #region Видимость тренда 9
+        public Visibility LineVisibility9
+        {
+            get { return (Visibility)GetValue(LineVisibility9Property); }
+            set { SetValue(LineVisibility9Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineVisibility9.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineVisibility9Property =
+            DependencyProperty.Register("LineVisibility9", typeof(Visibility), typeof(TrendControl), new PropertyMetadata(Visibility.Visible));
+        #endregion
+
+        #region Навзание тренда 9
+        public string LineTitle9
+        {
+            get { return (string)GetValue(LineTitle9Property); }
+            set { SetValue(LineTitle9Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineTitle9.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineTitle9Property =
+            DependencyProperty.Register("LineTitle9", typeof(string), typeof(TrendControl), new PropertyMetadata(""));
+        #endregion
+
+        #region Видимость тренда 10
+        public Visibility LineVisibility10
+        {
+            get { return (Visibility)GetValue(LineVisibility10Property); }
+            set { SetValue(LineVisibility10Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineVisibility9.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineVisibility10Property =
+            DependencyProperty.Register("LineVisibility10", typeof(Visibility), typeof(TrendControl), new PropertyMetadata(Visibility.Visible));
+        #endregion
+
+        #region Навзание тренда 10
+        public string LineTitle10
+        {
+            get { return (string)GetValue(LineTitle10Property); }
+            set { SetValue(LineTitle10Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineTitle10.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineTitle10Property =
+            DependencyProperty.Register("LineTitle10", typeof(string), typeof(TrendControl), new PropertyMetadata(""));
+        #endregion
     }
 }
