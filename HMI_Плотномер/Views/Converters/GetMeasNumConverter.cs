2f18130b63f43aa012080bf9193f7a2ed00075cf
simkaa47 <simkaa7@gmail.com>
2022-09-29 14:57:55 +0300

    Реализованы команды на удаление и редактирование  ЕИ

--- HMI_Плотномер/Views/Converters/GetMeasNumConverter.cs
+++ HMI_Плотномер/Views/Converters/GetMeasNumConverter.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Data;
 
-namespace IDensity.Core.Views.Converters
+namespace IDensity.Views.Converters
 {
     public class GetMeasNumConverter : IMultiValueConverter
     {
