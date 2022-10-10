f7319ae24dcfb2627be1d17d35bf24b0515cef21
konvels <90443282+konvels@users.noreply.github.com>
2022-10-10 17:33:47 +0300

    Создан User Control для параметра с ЕИ

---
+++ HMI_Плотномер/Views/Resourses/UserControls/MeasUnitTextParameter.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace IDensity.Views.Resourses.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для MeasUnitTextParameter.xaml
+    /// </summary>
+    public partial class MeasUnitTextParameter : CommandExtention
+    {
+        public MeasUnitTextParameter()
+        {
+            InitializeComponent();
+        }
+    }
+}
