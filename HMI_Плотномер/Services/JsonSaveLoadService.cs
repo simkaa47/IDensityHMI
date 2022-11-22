ba0770dfe1b70e22ada5e06a20d2284c9909d834
konvels <90443282+konvels@users.noreply.github.com>
2022-11-22 16:36:08 +0300

    Сохранение json при помощи Open File Dialog

--- HMI_Плотномер/Services/JsonSaveLoadService.cs
+++ HMI_Плотномер/Services/JsonSaveLoadService.cs
@@ -1,4 +1,5 @@
 ﻿using IDensity.Models;
+using Microsoft.Win32;
 using System;
 using System.IO;
 using System.Runtime.Serialization.Json;
@@ -9,8 +10,25 @@ namespace IDensity.Core.Services
     {
         public static void Save(object writeObject, Type type)
         {
+            string path = string.Empty;
+            OpenFileDialog fileDialog = new OpenFileDialog()
+            {
+                CheckFileExists = false,
+                CheckPathExists = true,
+                Multiselect = true,
+                Title = "Выберите файл"
+            };
+            fileDialog.Filter = "  Текстовые файлы (*.json)|*.json";
+            Nullable<bool> dialogOK = fileDialog.ShowDialog();
+
+
+            if (dialogOK == true)
+            {
+                path = fileDialog.FileName;
+
+            }
             var jsonFormatter = new DataContractJsonSerializer(type);
-            using (var file = new FileStream($"mainModel.json", FileMode.OpenOrCreate))
+            using (var file = new FileStream(path, FileMode.OpenOrCreate))
             {
                 jsonFormatter.WriteObject(file, writeObject);
             }
