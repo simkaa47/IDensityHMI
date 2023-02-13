16a6394b60a434d30d66e80f1a5a097f5009265d
konvels <90443282+konvels@users.noreply.github.com>
2023-02-13 14:53:59 +0300

    Added Trend Sett control

---
+++ HMI_Плотномер/Views/Resourses/UserControls/TrendSettingsControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace IDensity.Core.Views.Resourses.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для TrendSettingsControl.xaml
+    /// </summary>
+    public partial class TrendSettingsControl : UserControl
+    {
+        public TrendSettingsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
