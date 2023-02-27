aa6014b0667bd9f42ce2e1b8f2326f09991042b1
simkaa47 <simkaa7@gmail.com>
2023-02-27 15:49:54 +0300

    Added MeasUnitTextParameter

---
+++ HMI_Плотномер/Views/UserControls/Parameters/MeasUnitParameterText.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IDensity.Core.Views.UserControls.Parameters
+{
+    /// <summary>
+    /// Interaction logic for MeasUnitParameterText.xaml
+    /// </summary>
+    public partial class MeasUnitParameterText : UserControl
+    {
+        public MeasUnitParameterText()
+        {
+            InitializeComponent();
+        }
+    }
+}
