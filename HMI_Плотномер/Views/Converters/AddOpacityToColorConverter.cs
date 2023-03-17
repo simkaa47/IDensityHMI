a303385a40e4dfc23562030c9ef14302fdce8791
konvels <90443282+konvels@users.noreply.github.com>
2023-03-17 15:59:43 +0300

    Event style was changed

---
+++ HMI_Плотномер/Views/Converters/AddOpacityToColorConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Media;
+
+namespace IDensity.Core.Views.Converters
+{
+    internal class AddOpacityToColorConverter: Converter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is SolidColorBrush brush)) return null;
+            var copy = brush.Color;
+            copy.A = 100;
+            return new SolidColorBrush(copy);
+        }
+    }
+}
