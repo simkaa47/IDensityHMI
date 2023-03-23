ef47d3f05cdd5b9e698995e43be46dd62d8d6326
konvels <90443282+konvels@users.noreply.github.com>
2023-03-23 13:26:47 +0300

    added numeric converter

---
+++ HMI_Плотномер/Views/Converters/MultiplyOnNumberConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace IDensity.Core.Views.Converters
+{
+    class MultiplyOnNumberConverter: Converter
+    {
+        public float Offset { get; set; }
+        public MultiplyOnNumberConverter()
+        {
+           
+        }
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            float temp = 0;
+            if (value is null) return 0;
+            if(float.TryParse(value.ToString(), out temp)) return 0;
+            float x = temp;
+            if (parameter is null) return 0;
+            if (float.TryParse(parameter.ToString(), out temp)) return 0;
+            float k = temp;
+            return k * x + Offset;
+        }
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            float temp = 0;
+            if (value is null) return 0;
+            if (float.TryParse(value.ToString(), out temp)) return 0;
+            float x = temp;
+            if (parameter is null) return 0;
+            if (float.TryParse(parameter.ToString(), out temp)) return 0;
+            float k = temp;
+            return (x - Offset) / k;
+        }
+    }
+}
