f97eabce6723dfca21e8458e33cc4d21432229a3
konvels <90443282+konvels@users.noreply.github.com>
2023-04-05 16:58:10 +0300

    В мастере поверки прибора был поменян источник информации для векрсии HMI прибора

--- HMI_Плотномер/ViewModels/MasrerSettings/CheckMasterVm.cs
+++ HMI_Плотномер/ViewModels/MasrerSettings/CheckMasterVm.cs
@@ -271,8 +271,7 @@ namespace IDensity.ViewModels.MasrerSettings
         }
 
         void GetDeviceInformation()
-        {            
-            DeviceInfo.HmiSoftwareNumber = App.VersionNumber;
+        {                        
             DeviceInfo.SerialNumber = VM.mainModel.SerialNumber.Value;
         }
 
