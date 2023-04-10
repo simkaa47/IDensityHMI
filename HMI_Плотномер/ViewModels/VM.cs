1044996acece163b375be4ce48169bd2fb50302d
konvels <90443282+konvels@users.noreply.github.com>
2023-04-10 17:17:17 +0300

    Логирование, отображение трендов, результатов измерений перенесено в отдельную ViewModel

--- HMI_Плотномер/ViewModels/VM.cs
+++ HMI_Плотномер/ViewModels/VM.cs
@@ -1,32 +1,25 @@
-﻿using IDensity.AddClasses;
-using IDensity.AddClasses.EventHistory;
+﻿using IDensity.AddClasses.EventHistory;
 using IDensity.Core.Models.Events;
-using IDensity.Core.Models.Trends;
 using IDensity.Core.Models.Users;
+using IDensity.Core.ViewModels;
 using IDensity.Core.ViewModels.MeasUnits;
 using IDensity.DataAccess;
-using IDensity.DataAccess.Models;
-using IDensity.DataAccess.Repositories;
 using IDensity.Models;
 using IDensity.Models.SQL;
 using IDensity.Services.ComminicationServices;
-using IDensity.Services.InitServices;
 using IDensity.Services.SQL;
 using IDensity.ViewModels.Commands;
 using IDensity.ViewModels.MasrerSettings;
 using IDensity.ViewModels.SdCard;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.IO.Ports;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 
 namespace IDensity.ViewModels
@@ -36,9 +29,7 @@ namespace IDensity.ViewModels
     /// </summary>
     public class VM : PropertyChangedBase
     {
-        #region Репозитории
-        private  EfRepository<MeasResultLog> _measResultRepository;
-        #endregion
+
 
         #region Дочерние VM
         public CommonSettingsVm CommonSettingsVm { get; private set; }
@@ -49,13 +40,14 @@ namespace IDensity.ViewModels
         public MeasProcessVm MeasProcessVm { get; private set; }
         public AnalogVm AnalogVm { get; private set; }
         public MeasUnitVm MeasUnitSettingsVm { get; private set; }
+        public MeasResultsViewModel MeasResultsVM { get; private set; }
         #endregion
 
         #region Cервисы
         public CommunicationService CommService { get; private set; }
 
         #endregion
-        
+
         /// <summary>
         /// Инициализация дочерних Vm
         /// </summary>
@@ -69,8 +61,9 @@ namespace IDensity.ViewModels
             SdCardVm = new SdCardVm(this);
             AnalogVm = new AnalogVm(this);
             MeasUnitSettingsVm = new MeasUnitVm(this);
+            MeasResultsVM = new MeasResultsViewModel(this);
             CommService.Tcp.TcpEvent += WriteTcpLog;
-            _measResultRepository = new EfRepository<MeasResultLog>();
+            
 
 
         }
@@ -79,12 +72,11 @@ namespace IDensity.ViewModels
         void ServicesInit()
         {
             CommService = new CommunicationService(mainModel);
-            CommService.UpdateDataEvent += AddDataToCollection;
             CommService.MainProcessExecute();
 
         }
         #endregion
-        
+
         async void WriteTcpLog(string message)
         {
             try
@@ -96,12 +88,12 @@ namespace IDensity.ViewModels
             }
             catch (Exception)
             {
-                
-            }           
+
+            }
         }
 
         #region Версия ПО
-        public string SoftVersion { get; private set; } = Assembly.GetExecutingAssembly().GetName().Version.ToString(); 
+        public string SoftVersion { get; private set; } = Assembly.GetExecutingAssembly().GetName().Version.ToString();
         #endregion
 
 
@@ -109,9 +101,9 @@ namespace IDensity.ViewModels
 
         #region Текущий пользователь
         User _curUser;
-        public User CurUser 
-        { 
-            get => _curUser; 
+        public User CurUser
+        {
+            get => _curUser;
             set
             {
                 if (Set(ref _curUser, value))
@@ -152,33 +144,23 @@ namespace IDensity.ViewModels
             const string caption = "Закрытие приложения";
 
             var result = MessageBox.Show(message, caption, MessageBoxButton.YesNo, MessageBoxImage.Question);
-            if(result == MessageBoxResult.Yes)
+            if (result == MessageBoxResult.Yes)
             {
                 Application.Current.Shutdown();
-            }                                
-            
+            }
+
         }, o => true));
         #endregion        
 
         #region Команда вкл-выкл измерения
         RelayCommand _switchMeasCommand;
         public RelayCommand SwitchMeasCommand { get => _switchMeasCommand ?? (_switchMeasCommand = new RelayCommand(o => CommService.SwitchMeas(), o => true)); }
-        #endregion
-
-        #region Команда плказать архивный тренд
-        RelayCommand _showArchivalTrendCommand;
-        public RelayCommand ShowArchivalTrendCommand { get => _showArchivalTrendCommand ?? (_showArchivalTrendCommand = new RelayCommand(o => ShowArchivalTrend(), o => true)); }
-        #endregion
+        #endregion        
 
         #region Команда "Логаут"
         RelayCommand _logoutCommand;
         public RelayCommand LogoutCommand { get => _logoutCommand ?? (_logoutCommand = new RelayCommand(o => CurUser = null, o => true)); }
-        #endregion
-
-        #region Команда "Записать архивный тренд в файл"
-        RelayCommand _writeLogCommand;
-        public RelayCommand WriteLogCommand { get => _writeLogCommand ?? (_writeLogCommand = new RelayCommand(o => WriteArchivalTrendToText(), o => true)); }
-        #endregion                   
+        #endregion                         
 
         #region Команды измерения даты-времени
         #region установить RTC пользователя
@@ -222,10 +204,8 @@ namespace IDensity.ViewModels
                 Events.EventExecute += AddHistoryItem;
                 _selectedEventItems.Filter += OnEventsFiltered;
                 _selectedEventItems.SortDescriptions.Add(new SortDescription("EventTime", ListSortDirection.Descending));
-                GetMeasDates();
                 ServicesInit();
                 InitVm();
-                TrendInit();                
             }
         }
 
@@ -250,242 +230,7 @@ namespace IDensity.ViewModels
         #region Cписок доступных Com портов 
         string[] _comPorts;
         public string[] ComPorts { get => _comPorts ?? (_comPorts = SerialPort.GetPortNames()); set => Set(ref _comPorts, value); }
-        #endregion
-
-       
-
-        #region Данные для текущего тренда
-        #region Алгоритм интерполяции
-        int _interpolIndex = 3;
-        public int InterpolIndex { get => _interpolIndex; set { Set(ref _interpolIndex, value); } }
-        #endregion
-
-        void TrendInit()
-        {
-            _trendSettings = XmlInit.ClassInit<GraphSettings>();
-            _trendsVisible = XmlInit.ClassInit<TrendVisible>();
-        }
-        #region Настройки тренда
-        GraphSettings _trendSettings;
-        public GraphSettings TrendSettings => _trendSettings;
-        #endregion
-
-        ObservableCollection<MeasResultLog> _plotCollection;
-        public ObservableCollection<MeasResultLog> PlotCollection
-        {
-            get => _plotCollection ?? (_plotCollection = new ObservableCollection<MeasResultLog>());
-        }
-        #endregion
-
-        #region Настройки видимости графиков
-        private TrendVisible _trendsVisible;
-        public TrendVisible TrendsVisible => _trendsVisible;
         #endregion 
-
-        #region Данные для архивного тренда
-        #region Коллекция
-        IEnumerable<MeasResultLog> _archivalDataPotnts = new List<MeasResultLog> { new MeasResultLog { Time =new DateTime(2020,1,1) }, new MeasResultLog { Time = DateTime.Now } };
-        public IEnumerable<MeasResultLog> ArchivalDataPotnts { get => _archivalDataPotnts; private set { Set(ref _archivalDataPotnts, value); } }
-        #endregion
-
-        #region Настройки
-        #region Стартовая точка отображаемого тренда
-        DateTime _displayDateStart = DateTime.Today.AddDays(-1);
-        public DateTime DisplayDateStart
-        {
-            get => _displayDateStart;
-            set
-            {
-                Set(ref _displayDateStart, value);
-                if (value >= DisplayDateEnd) DisplayDateEnd = value.AddMinutes(1);
-                if (value.AddDays(2) < DisplayDateEnd) DisplayDateEnd = value.AddDays(2);
-            }
-        }
-        #endregion
-        #region Конечная точка отображаемого тренда
-        DateTime _displayDateEnd = DateTime.Today;
-        public DateTime DisplayDateEnd
-        {
-            get => _displayDateEnd;
-            set
-            {
-                Set(ref _displayDateEnd, value);
-                if (value <= DisplayDateStart) DisplayDateStart = value.AddMinutes(-1);
-                if (value.AddDays(-2) > DisplayDateStart) DisplayDateStart = value.AddDays(-2);
-            }
-        }
-        #endregion
-        #region Путь к логируемому файлу
-        string _logPath="Выберите файл";
-        public string LogPath { get => _logPath; set => Set(ref _logPath, value); }
-        #endregion
-
-        #region Список дат, когда происходили измерения
-        List<BlackOutData> MeasDates { get; set; } = new List<BlackOutData>();
-        void GetMeasDates()
-        {
-            MeasDates = SqlMethods.ReadFromSql<BlackOutData>($"SELECT DISTINCT date(time) FROM TimePoints;");
-        }
-        #endregion
-
-        #endregion
-
-        #region Состояние загрузки из ДБ
-        bool _archivalTrendDownloading;
-        public bool ArchivalTrendDownloading { get => _archivalTrendDownloading; private set { Set(ref _archivalTrendDownloading, value); } }
-        #endregion
-
-        #region Состояние загрузки в текстовый файл
-        bool _archivalTrendUploading;
-        public bool ArchivalTrendUploading { get => _archivalTrendUploading; private set { Set(ref _archivalTrendUploading, value); } }
-        #endregion
-
-        #endregion        
-
-        #region Вывести данные из БД
-        async void ShowArchivalTrend()
-        {
-            try
-            {
-                ArchivalTrendDownloading = true;
-                await Task.Run(() =>
-                {                    
-                    var list = _measResultRepository.GetWhere(m => m.Time >= DisplayDateStart && m.Time <= DisplayDateEnd);
-                    for (int j = 0; j < 2; j++)
-                    {
-                        var unit = GetCoeff(mainModel.MeasResults[j].MeasUnitMemoryId);
-                        foreach (var point in list)
-                        {
-                            if (j == 0)
-                            {
-                                point.CurValue1 = point.CurValue1 * unit.K + unit.Offset;
-                                point.AvgValue1 = point.AvgValue1 * unit.K + unit.Offset;
-                            }
-                            else
-                            {
-                                point.CurValue2 = point.CurValue2 * unit.K + unit.Offset;
-                                point.AvgValue2 = point.AvgValue2 * unit.K + unit.Offset;
-                            }
-                        }
-                        MeasResultMeasUnits[j] = unit;
-                    }
-                    ArchivalDataPotnts = list;
-                ArchivalTrendDownloading = false;
-            });
-
-            }
-            catch (Exception)
-            {
-
-            }
-        }
-        #endregion        
-
-        #region Добавление данных в график
-        void AddDataToCollection()
-        {
-            if (!(mainModel.CycleMeasStatus.Value || TrendSettings.WriteIfNoMeasState)) return;
-            App.Current?.Dispatcher?.Invoke(() =>
-                {
-                    var tp = new MeasResultLog
-                    {
-                        Time = DateTime.Now,
-                        Pulses = mainModel.MeasResults[0].CounterValue.Value,
-                        CurValue1 = mainModel.MeasResults[0].PhysValueCur.Value,
-                        AvgValue1 = mainModel.MeasResults[0].PhysValueAvg.Value,
-                        CurValue2 = mainModel.MeasResults[1].PhysValueCur.Value,
-                        AvgValue2 = mainModel.MeasResults[1].PhysValueAvg.Value,
-                        Current1 = mainModel.AnalogGroups[0].AI.AdcValue.Value,
-                        Current2 = mainModel.AnalogGroups[1].AI.AdcValue.Value,
-                        HvValue1 = mainModel.TelemetryHV.VoltageCurOut.Value,
-                        Temperature = mainModel.TempTelemetry.TempInternal.Value
-                    };
-                    _measResultRepository.Add(tp);             
-                    tp.CurValue1 = mainModel.MeasResults[0].PhysValueCur.Value * MeasResultMeasUnits[0].K + MeasResultMeasUnits[0].Offset;
-                    tp.CurValue2 = mainModel.MeasResults[0].PhysValueAvg.Value * MeasResultMeasUnits[0].K + MeasResultMeasUnits[0].Offset;
-                    tp.AvgValue1 = mainModel.MeasResults[1].PhysValueCur.Value * MeasResultMeasUnits[1].K + MeasResultMeasUnits[1].Offset;
-                    tp.AvgValue2 = mainModel.MeasResults[1].PhysValueAvg.Value * MeasResultMeasUnits[1].K + MeasResultMeasUnits[1].Offset;
-                    PlotCollection.Add(tp);
-                    int i = 0;
-                    while (PlotCollection.Count > 0 && PlotCollection[0].Time < DateTime.Now.AddMinutes(TrendSettings.PlotTime * (-1)) && i < 10)
-                    {
-                        PlotCollection.RemoveAt(0);
-                        i++;
-                    }
-                    for (int j = 0; j < 2; j++)
-                    {                        
-                        var unit = GetCoeff(mainModel.MeasResults[j].MeasUnitMemoryId);
-                        if (!(MeasUnit.CompareMeasUnits(unit, MeasResultMeasUnits[j])))
-                        {                            
-                            foreach (var point in PlotCollection)
-                            {
-                                if (j == 0)
-                                {
-                                    point.CurValue1 = ((point.CurValue1 - MeasResultMeasUnits[j].Offset) / MeasResultMeasUnits[j].K) * unit.K + unit.Offset;
-                                    point.AvgValue1 = ((point.AvgValue1 - MeasResultMeasUnits[j].Offset) / MeasResultMeasUnits[j].K) * unit.K + unit.Offset;
-                                }
-                                else
-                                {
-                                    point.CurValue2 = ((point.CurValue2 - MeasResultMeasUnits[j].Offset) / MeasResultMeasUnits[j].K) * unit.K + unit.Offset;
-                                    point.AvgValue2 = ((point.AvgValue2 - MeasResultMeasUnits[j].Offset) / MeasResultMeasUnits[j].K) * unit.K + unit.Offset;
-                                }
-                            }
-                            MeasResultMeasUnits[j] = unit;
-                        }
-                    }
-                    
-                });
-
-        }
-        #endregion        
-
-        #region Время усреднения для пользователя(запись)
-        uint _avgTimeWrite = 1;
-        public uint AvgTimeWrite
-        {
-            get => _avgTimeWrite;
-            set
-            {
-                if (value > 0) Set(ref _avgTimeWrite, value);
-            }
-        }
-        #endregion
-
-        #region Запись в файл
-        async void WriteArchivalTrendToText()
-        {
-            if (ArchivalTrendUploading) return;
-            if (ArchivalDataPotnts == null) return;
-            try
-            {
-                ArchivalTrendUploading = true;
-                await Task.Run(() =>
-                {
-                    StringBuilder builder = new StringBuilder();
-                    builder.Append("Дата Время" + "\t" + "Счетчик" + "\t" + "Процесс 0: мгновенная ФВ" + "\t"
-                             + "Процесс 0: усредненная ФВ" + "\t"
-                             + "Процесс 1: мгновенная ФВ" + "\t" + "Процесс 1: усредненная ФВ" + "\t"
-                             + "Ток AI0, мкA" + "\t" + "Ток AI1, мкA" + "\t" + "Значение HV out, V" + "\t" + "Температура" + "\n");
-                    foreach (var item in ArchivalDataPotnts)
-                    {
-                        builder.Append(item.Time.ToString("dd/MM/yyyy HH:mm:ss:f") + "\t" + item.Pulses.ToString("0.000") + "\t" + item.CurValue1.ToString("0.000") + "\t"
-                             + item.AvgValue1.ToString("0.000") + "\t" 
-                             + item.CurValue2.ToString("0.000") + "\t" + item.AvgValue2.ToString("0.000") + "\t"
-                             + item.Current1.ToString("0.000") + "\t" + item.Current2.ToString("0.000") + "\t" + item.HvValue1.ToString("0.000") + "\t" + item.Temperature.ToString("0.000") + "\n");
-                    }
-                    using (StreamWriter sw = new StreamWriter(LogPath, false, System.Text.Encoding.Default))
-                    {
-                        sw.WriteLine(builder.ToString());
-                    }
-                });
-                ArchivalTrendUploading = false;
-            }
-            catch (Exception ex)
-            {
-
-            }
-        }
-        #endregion        
 
         #region События
         #region Фильтрация событий
@@ -644,54 +389,6 @@ namespace IDensity.ViewModels
         }
         #endregion
 
-
-        void UpadateDates(object obj)
-        {
-            if (!(obj is Calendar calendar)) return;
-            DateTime startDate = new DateTime(calendar.DisplayDate.Year, calendar.DisplayDate.Month, 1);
-            var enabledDates = MeasDates.Where(ms => ms.Time >= startDate && ms.Time <= startDate.AddMonths(1)).Select(ms => ms.Time);
-            calendar.DisplayDateEnd = startDate.AddMonths(1);
-            var tempDate = startDate;
-            calendar.BlackoutDates.Clear();
-            while (tempDate <= calendar.DisplayDateEnd)
-            {
-                if (tempDate != calendar.SelectedDate && !enabledDates.Any(dt => dt == tempDate))
-                    calendar.BlackoutDates.Add(new CalendarDateRange(tempDate));
-                tempDate = tempDate.AddDays(1);
-            }
-        }
-        private RelayCommand _selectDatesCommand;
-
-        public RelayCommand SelectDatesCommand
-        {
-            get { return _selectDatesCommand ?? (_selectDatesCommand = new RelayCommand(obj => UpadateDates(obj), obj => true)); }
-
-        }
         #endregion
-
-        /// <summary>
-        /// Метод для расчета 
-        /// </summary>
-        /// <param name="memoryId"></param>
-        /// <returns></returns>
-        private MeasUnit GetCoeff(string memoryId)
-        {
-            MeasUnitMemory memory = null;
-            MeasUnit measUnit = null;
-            using (var _measUnitMemoryRepository = new EfRepository<MeasUnitMemory>())
-            {
-                memory = _measUnitMemoryRepository.GetAll().Where(m => m.Name == memoryId).FirstOrDefault();
-            }
-            if (memory is null) return new MeasUnit();
-            using (var _measUnitRepository = new EfRepository<MeasUnit>())
-            {
-                measUnit = _measUnitRepository.GetAll().Where(mu => mu.Id == memory.MeasUnitId).FirstOrDefault();
-            }  
-            if (measUnit is null) return new MeasUnit();
-            return measUnit;
-        }
-
-        MeasUnit[] MeasResultMeasUnits { get; } = Enumerable.Range(0, 2).Select(i => new MeasUnit()).ToArray();
-
     }
 }
