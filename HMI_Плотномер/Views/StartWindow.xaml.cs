949b559c7e6e090fdfc85418c1379df86289dcf8
Serafim Prilezhaev <simkaa7@gmail.com>
2023-05-17 15:15:22 +0300

    Добавлена стартовая страница

---
+++ HMI_Плотномер/Views/StartWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IDensity.Core.Views
+{
+    /// <summary>
+    /// Interaction logic for StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+            SoftVersion.Text = "VERSION " + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            ShowNameButton.IsEnabled = false;
+            var main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+    }
+}
